287575b3df7dcb7e32ba8cc2e177b0e57f6bc918
Hikari Kobe <h.kobe712@gmail.com>
2024-01-16 11:51:49 -0800

    styles: added base containers

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,26 @@ import './styles/global.scss';
 import './App.scss';
 
 function App() {
-  return <p className='test'>testテスト</p>;
+  return (
+    <div className='container'>
+      <div className='title_container'>
+        <h1>track-cafe</h1>
+        <p>お好きなメニューを選択してください</p>
+      </div>
+      <div className='contents'>
+        <div className='menu_container'>
+          <div>
+            <button>coffee</button>
+            <button>coffee</button>
+            <button>coffee</button>
+            <button>coffee</button>
+            <button>coffee</button>
+          </div>
+        </div>
+        <div>Display Total component</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
